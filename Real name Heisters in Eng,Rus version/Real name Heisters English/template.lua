73291486c7ba44b597fdbc794ba7200fa6721a44
zadiraka <zadirakadeep@gmail.com>
2018-11-10 21:08:25 +0300

    Add files via upload

---
+++ Real name Heisters in Eng,Rus version/Real name Heisters English/template.lua
@@ -0,0 +1,87 @@
+﻿local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_old_hoxton" and "James Hoxworth" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_spanish" and "Nicolas" or text_original(self, string_id, ...)	
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_jowi" and "Jonathan Baba Yaga The Boogeyman" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_female_1" and "Rochelle" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_dragan" and "Mr.Zubovic" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_jacket" and "Richard" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+	                return string_id == "menu_bonnie" and "Bonnie McGee" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_sokol" and "Sergei Kozak" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_jimmy" and "James" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+                    return string_id == "menu_sydney" and "Kelli King" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+                   return string_id == "menu_sydney" and "Kelli King" or text_original(self, string_id, ...) 
+end	
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	 return string_id == "menu_wild" and "Tom Bishop The Devil" or text_original(self, string_id, ...)               
+end	
+
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	  return string_id == "menu_terry" and "Tony Montana Antonio" or text_original(self, string_id, ...)              
+end	
+	            
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	 return string_id == "menu_max" and "Machetazo Antonio Benítez Rodríguez" or text_original(self, string_id, ...)               
+end	
+	 local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	 return string_id == "menu_myh" and "August Lindenhurst" or text_original(self, string_id, ...)               
+end	
+	 local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	   return string_id == "menu_ecp_male" and "Ethan Klein" or text_original(self, string_id, ...)             
+end	
+	 local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	  return string_id == "menu_ecp_female" and "Hila Klein Hila Hakmon" or text_original(self, string_id, ...)              
+end	
+	 local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	  return string_id == "menu_ecp_male" and "Ethan Klein" or text_original(self, string_id, ...)              
+end	
+	 				
---
+++ Real name Heisters in Eng,Rus version/Real name Heisters Russian/template.lua
@@ -0,0 +1,82 @@
+﻿local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_old_hoxton" and "Джеймс Хоксворт" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_spanish" and "Николос" or text_original(self, string_id, ...)	
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_jowi" and "Джонатан Баба Яга бугимен" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_female_1" and "Рошель" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_dragan" and "Мр.Зубовик" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	                return string_id == "menu_jacket" and "Ричард" or text_original(self, string_id, ...)
+end					
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+	                return string_id == "menu_bonnie" and "Бонни МакГии" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_sokol" and "Сергей Козак" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...) 
+                    return string_id == "menu_jimmy" and "Джеймс" or text_original(self, string_id, ...)
+end
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+                    return string_id == "menu_sydney" and "Келли Кинг" or text_original(self, string_id, ...)
+end					
+
+	
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	 return string_id == "menu_wild" and "Том Бишоп Сатана" or text_original(self, string_id, ...)               
+end	
+
+
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	  return string_id == "menu_terry" and "Тони Монтана Антонио" or text_original(self, string_id, ...)              
+end	
+	            
+local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	 return string_id == "menu_max" and "Мачетозо Антонио Бенитес Родригес" or text_original(self, string_id, ...)               
+end	
+	 local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	 return string_id == "menu_myh" and "Август Линденхерст" or text_original(self, string_id, ...)               
+end	
+	 local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	   return string_id == "menu_ecp_male" and "Итан Клеин" or text_original(self, string_id, ...)             
+end	
+	 local text_original = LocalizationManager.text
+    function LocalizationManager:text(string_id, ...)
+	  return string_id == "menu_ecp_female" and "Хила Клейн Хила Хакмон" or text_original(self, string_id, ...)              
+end	
+            
+	
+	 				
